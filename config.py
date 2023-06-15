19a39b44836bd149055aa9f8bd7b486af96ea256
GenaBeton87 <svyateg@mail.ru>
2023-06-15 12:35:35 +0300

    konfig

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-comunity_token = 'vk1.a.-JQ1sRmdRH-fe_y9tCJfXIiGPF4FcSNRwimbkjFsCWwsSLqVpFKArpuHkeTOYBZuyswNNNG82y_LNt1Up8NvjqU1bb26lVR2xXxdmVSHIlnAFdTCEab3KpVHyvn13qBrdLdj12XhsMz7vTU8SEUSF_CQYhihgQ8NQdsVtGgv3jqFc70ib7jRdR7LpHLNSev6NWJkwK8xypmnsLauo_3fEg'
-acces_token = 'vk1.a.CqipYkqtbzzrdvwUPjRCABdM0H_oNsBV6GuN1LGLoUMcsuquC6QIMSvI_z2qRTulNSAIJiMTXuwyBIvSLgxpS8In4GLkqSCsp1vsxx1K-_o_n7tlnET0zIzFwnQLjs1zKEvvI2jtHufSSiaoSdWcdODRWq2Xak-PYdFbEu7zJljFfYP_KnUnXw9Cj2kn3kDACNa3eXBK-SugsjXNBlt3xA'
-db_url_object = "postgresql://postgres:16092020@localhost:5432/diplom"
+comunity_token = ''
+acces_token = ''
+db_url_object = "postgresql://postgres:3333@localhost:3333/diplom"
